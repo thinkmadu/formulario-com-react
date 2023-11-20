86fc39b68988613ed5b1d8705f765af74a1afe1c
MaduRa <maria.silva29@aluno.uepb.edu.br>
2023-11-19 21:02:03 -0300

    Apagando arquivos não utilizados

--- formulario-com-react/src/About.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function About() {
-  return <h2>Sobre</h2>;
-}
-
-export default About;
--- formulario-com-react/src/NoMatch.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function NoMatch() {
-  return <h2>404 - Página não encontrada</h2>;
-}
-
-export default NoMatch;
